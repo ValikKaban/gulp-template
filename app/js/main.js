20c1e34b75911d78afef5aa882c54e86d88a6ff1
ValikKaban <Valjainges125929mailkaban.com@gmail.com>
2020-02-26 18:55:21 +0200

    create simple gulp project

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+let randomize = (min,max) => Math.round(min + Math.random()*(max-min))
+
+function colors() {
+    const r = randomize(0,255);
+    const g = randomize(0,255);
+    const b = randomize(0,255);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  console.log(colors())
